60a895ffdc20c777150e0f7083532c9a9272583f
bhargavmane1802 <bhargavmane18@gmail.com>
2026-02-14 21:06:32 +0530

    Time: 5 ms (56.85%), Space: 20.1 MB (27.31%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(n);
+        vector<int>in(n);
+        for(auto x:prerequisites){
+            in[x[0]]++;
+            adj[x[1]].push_back(x[0]);
+        }
+        queue<int>q;
+        vector<int>visited(n);
+        for(int i=0;i<n;i++){
+            if(in[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int index=q.front();
+            q.pop();
+            for(auto i:adj[index]){
+                in[i]--;
+                if(in[i]==0)q.push(i);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(in[i]!=0)return false;
+        }
+        return true;
+    }
+};
