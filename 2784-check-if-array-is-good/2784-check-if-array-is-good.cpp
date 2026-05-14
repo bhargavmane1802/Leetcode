31e7e27cf502d56c11818a0cc1de949f91cbe7e9
Bhargav Mane <bhargavmane18@gmail.com>
2026-05-14 20:11:39 +0530

    Time: 0 ms (100%), Space: 27.1 MB (36.95%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool isGood(vector<int>& nums) {
         // brute force 
-        sort(nums.begin(),nums.end());
         int n=nums.size();
-        if (n==1)return false;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]!=i+1)return false;
+        vector<int>check(n);
+        if(n==1)return false;
+        for(int i:nums){
+            if(i>=n)return false;
+            check[i]++;
+            if((i==n-1 && check[i]>2)||(i!=n-1 && check[i]>1))return false;
         }
-        if(nums[n-1]==n-1)return true;
-        return false;
+        return true;
     }
 };
