1ac52d2030d06f16000370ef7f015a16e6bc8131
Bhargav Mane <bhargavmane18@gmail.com>
2026-05-17 14:54:13 +0530

    Time: 51 ms (6.37%), Space: 56.4 MB (5.2%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -6,17 +6,17 @@ public:
         queue<int>q;
         vector<int>visited(n);
         for(int i=0;i<n;i++){
-            if(0==arr[i]){q.push(i);visited[i]=1;}
-            
             int r=i+arr[i];
-            if(r<n)mp[r].push_back(i);
+            if(r<n)mp[i].push_back(r);
             r=i-arr[i];
-            if(r>=0)mp[r].push_back(i);
+            if(r>=0)mp[i].push_back(r);
         }
+        q.push(start);
+        visited[start]=1;
         while(!q.empty()){
             int top=q.front();
             q.pop();
-            if(top==start)return true;
+            if(arr[top]==0)return true;
             for(int i :mp[top]){
                 if(visited[i]==0){
                     visited[i]=1;
