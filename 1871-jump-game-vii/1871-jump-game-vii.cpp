913dcebc662b00197ce4fc382bac0a9a4ecf3516
Bhargav Mane <bhargavmane18@gmail.com>
2026-05-25 11:02:07 +0530

    Time: 12 ms (46.82%), Space: 23.2 MB (64.29%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -5,19 +5,16 @@ public:
         queue<int>q;
         if(n==1)return true;
         if(s[n-1]=='1')return false;
-        vector<int>visited(n);
         q.push(0);
         int m=0;
-        visited[0]=1;
         while(!q.empty()){
             int t=q.front();
             q.pop();
             int l=max(m,(t+minJump));
             while(l<n && l<=maxJump+t){
-                if(s[l]=='0' && visited[l]==0){
+                if(s[l]=='0'){
                     if((l)==n-1)return true;
                     q.push(l);
-                    visited[l]=1;
                 }
                 l++;
             }
