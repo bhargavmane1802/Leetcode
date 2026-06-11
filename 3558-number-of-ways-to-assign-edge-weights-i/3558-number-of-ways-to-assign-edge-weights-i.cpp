1e8f5d2a44d09ca73d707aff3ca19699263247a6
Bhargav Mane <bhargavmane18@gmail.com>
2026-06-11 11:25:37 +0530

    Time: 296 ms (51.01%), Space: 332.3 MB (85.24%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    static const int MOD = 1e9 + 7;
+
+    long long modPow(long long a, long long b) {
+        long long res = 1;
+        while (b) {
+            if (b & 1) res = (res * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+
+        vector<vector<int>> adj(n + 1);
+        for (auto &e : edges) {
+            int u = e[0], v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> depth(n + 1, -1);
+        queue<int> q;
+        q.push(1);
+        depth[1] = 0;
+
+        int maxDepth = 0;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            maxDepth = max(maxDepth, depth[u]);
+
+            for (int v : adj[u]) {
+                if (depth[v] == -1) {
+                    depth[v] = depth[u] + 1;
+                    q.push(v);
+                }
+            }
+        }
+        return (int)modPow(2, maxDepth - 1);
+    }
+};
