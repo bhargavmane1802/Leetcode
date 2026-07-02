a131dd41849c1963fd502cc57de9fe74f56f1a2d
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-02 23:32:54 +0530

    Time: 14 ms (94.14%), Space: 18 MB (72.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int a) {
-        vector<int>visited(a+1,INT_MAX);
-        visited[0]=1;
-        for(int i=0;i<a;i++){
-            if(visited[i]==INT_MAX)continue;
-            for(int c:coins){
-                if(i+c <=a){
-                    visited[i+c]=min(visited[i+c],visited[i]+1);
-                }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>temp(amount+1,INT_MAX-1);
+        temp[0]=0;
+        for(int j=0;j<coins.size();j++){
+            for(int i=coins[j];i<=amount;i++){
+                temp[i]=min(temp[i],temp[i-coins[j]]+1);
             }
         }
-        if(visited[a]==INT_MAX)return-1;
-        return visited[a]-1; 
+        if(temp[amount]==INT_MAX-1)return -1;
+        return temp[amount];
     }
 };
