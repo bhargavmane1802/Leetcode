a2b7897e1f195a369a75be52d4fafe3b18f6f23a
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-14 17:08:03 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.16%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==1)return x;
+        if(n==-1)return 1/x;
+        if(n==0)return 1;
+        if(n%2==0){
+            double a= myPow(x,n/2);
+            return a*a;
+        }
+        else {
+            double a= myPow(x,n/2);
+            if(n<0){
+               return a*a*(1/x); 
+            }
+            return a*a*x;
+        }
+    }
+};
