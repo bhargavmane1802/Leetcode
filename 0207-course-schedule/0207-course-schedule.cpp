2f708d3960a1e286bb89255513fdaf17744aaa4f
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-25 02:06:57 +0530

    Time: 2 ms (79.09%), Space: 20 MB (32.5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+    bool canFinish(int n, vector<vector<int>>& p) {
+        vector<int>inorder(n);
+        vector<int>visited(n,-1);
         vector<vector<int>>adj(n);
-        vector<int>in(n);
-        for(auto x:prerequisites){
-            in[x[0]]++;
+        for(auto x:p){
             adj[x[1]].push_back(x[0]);
+            inorder[x[0]]++;
         }
         queue<int>q;
         for(int i=0;i<n;i++){
-            if(in[i]==0) q.push(i);
+            if(inorder[i]==0){
+                q.push(i);
+                visited[i]=1;
+            }
         }
         while(!q.empty()){
-            int index=q.front();
+            int t=q.front();
             q.pop();
-            for(auto i:adj[index]){
-                in[i]--;
-                if(in[i]==0)q.push(i);
-                if(in[i]<0)return false;
+            for(auto x: adj[t]){
+                inorder[x]--;
+                if(inorder[x]==0){
+                    q.push(x);
+                    visited[x]=1;
+                }
             }
         }
         for(int i=0;i<n;i++){
-            if(in[i]!=0)return false;
+            if(visited[i]==-1)return false;
         }
         return true;
     }
