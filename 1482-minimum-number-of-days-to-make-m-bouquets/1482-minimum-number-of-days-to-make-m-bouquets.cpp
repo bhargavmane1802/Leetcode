d7825095c7967efdbb546c3e2f12e5293a86ab7c
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-25 15:44:55 +0530

    Time: 18 ms (84.18%), Space: 70.5 MB (79.72%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    int minDays(vector<int>& nums, int m, int k) {
-        int n=nums.size();
-        if(1LL*m*k>n)return -1;
-        int mx=0;
-        for(int i :nums)mx=max(mx,i);
-        int l=1;
-        int r=mx;
-        while(l<=r){
-            int days=l+(r-l)/2;
-            int x=0;
-            int count=m;
-            while(x<=n-k){
-                bool check=true;
-                for(int i=0;i<k;i++){
-                    if(nums[i+x]>days){
-                        check=false;
-                        x+=i+1;
+    bool solve(long long mid,vector<int>&b,int m,int k){
+        for(int i=0;i<=b.size()-k;i++){
+            if(m==0)break;
+            for(int j=i;j<i+k;j++){
+                if(b[j]>mid){
+                    i=j;
+                    break;
+                }
+                else{
+                    if(j==(i+k-1)){
+                        m--;
+                        i=i+k-1;
                         break;
                     }
                 }
-                if(check){
-                    count--;
-                    x+=k;
-                    if(count==0)break;
-                }
             }
-            if(count==0)r=days-1;
-            else l=days+1;
+        }
+        if(m==0)return true;
+        return false;
+
+    }
+
+    int minDays(vector<int>& b, int m, int k) {
+        long long n=b.size();
+        if(n<(1ll*m*k))return -1;
+        long long l=1;
+        long long r=b[0];
+        for(long long i:b){
+            r=max(r,i);
+        }
+        while(l<=r){
+            long long mid=l+(r-l)/2;
+            cout<<mid<<" ";
+            if(solve(mid,b,m,k)){
+                r=mid-1;
+            }
+            else l=mid+1;
         }
         return l;
     }
