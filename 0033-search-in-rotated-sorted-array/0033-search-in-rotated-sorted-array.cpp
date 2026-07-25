871a217c63cde5892a61891b09d829975e70ac78
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-25 14:35:48 +0530

    Time: 0 ms (100%), Space: 15.4 MB (36.36%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,12 +6,19 @@ public:
         while(l<=r){
             int mid=l+(r-l)/2;
             if(nums[mid]==target)return mid;
-            else if((nums[l]<nums[mid] && (nums[l]<=target && target<nums[mid]))|| (nums[l]>nums[mid] && (nums[l]<=target || nums[mid]>target))){
-                r=mid-1;
+            if(nums[l]<=nums[mid]){
+                if(nums[mid]>target && nums[l]<=target){
+                    r=mid-1;
+                }
+                else l=mid+1;
             }
-            else l=mid+1;
-            
+            else{
+                if(target>=nums[l] || target<nums[mid]){
+                    r=mid-1;
+                }
+                else l=mid+1;
+            }
         }
-        return -1;    
+        return -1;
     }
 };
