e77d2ec02f05cd2dcb7bbaaf3b57a802dc6ae971
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-29 15:27:22 +0530

    Time: 11 ms (79.4%), Space: 30.4 MB (32.18%) - LeetHub

--- 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
+++ 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-        int minOperations(string s1, string s2) {
-        if (s1 == "1" && s2 == "0") {
-            return -1;
-        }
-        int res = 0, n = s1.length();
-        for (int i = 0; i < n; i++) {
-            if (s1[i] == s2[i])
-                continue;
-            if (s1[i] == '0') {
-                res++;
-            } else if (i < n - 1) {
-                res += s1[i + 1] == '1' ? 1 : 2;
-                s1[i + 1] = '0';
-            } else {
-                res += 2;
+    int minOperations(string s1, string s2) {
+        int n=s1.size();
+        if(s1=="1" && s2=="0")return -1;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s1[i]==s2[i])continue;
+            else if(s1[i]=='0')ans++;
+            else {
+                if(i<n-1){
+                    if(s1[i+1]=='1')ans++;
+                    else ans+=2;
+                    s1[i+1]='0';
+                }
+                else{
+                    ans+=2;
+                }
             }
         }
-        return res;
+        return ans;
     }
 };
