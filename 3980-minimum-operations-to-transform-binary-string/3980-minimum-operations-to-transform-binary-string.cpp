7592486427d550c2bf4000689595155b29369e8d
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-29 15:08:19 +0530

    Time: 11 ms (79.4%), Space: 30.4 MB (59.23%) - LeetHub

---
+++ 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+        int minOperations(string s1, string s2) {
+        if (s1 == "1" && s2 == "0") {
+            return -1;
+        }
+        int res = 0, n = s1.length();
+        for (int i = 0; i < n; i++) {
+            if (s1[i] == s2[i])
+                continue;
+            if (s1[i] == '0') {
+                res++;
+            } else if (i < n - 1) {
+                res += s1[i + 1] == '1' ? 1 : 2;
+                s1[i + 1] = '0';
+            } else {
+                res += 2;
+            }
+        }
+        return res;
+    }
+};
