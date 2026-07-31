cac9de85a8739e15d0f082a7c869433c74a78211
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-01 01:08:26 +0530

    Time: 439 ms (29.2%), Space: 169.6 MB (17.87%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -11,39 +11,31 @@
  */
 class Solution {
 public:
-int ans=0;
-int sum=0;
-    bool solve(TreeNode*root,int &mx,int &mn){
-        if(!root){
-            mx=INT_MIN;
-            mn=INT_MAX;
-            sum=0;
-            return true;
+    vector<int> solve(TreeNode*root,int &ans){
+        int sum=root->val,m=root->val,mx=root->val;
+        int check=1;
+        if(root->left){
+            auto x=solve(root->left,ans);
+            sum+=x[0];
+            m=min(m,(min(x[2],x[1])));
+            mx=max(mx,max(x[1],x[2]));
+            if(x[3]==0 || root->val <=x[1] || root->val<=x[2])check=0;
         }
         
-        bool one=true;
-        if(!(solve(root->left,mx,mn) && root->val>mx)){
-            one=false;
+        if(root->right){
+            auto y=solve(root->right,ans);
+            sum+=y[0];
+            m=min(m,min(y[1],y[2]));
+            mx=max(mx,max(y[1],y[2]));
+            if(y[3]==0 || root->val>=y[1] || root->val>=y[2])check=0;
         }
-        int s=sum;
-        int mnn=mn;
-        bool two=true;
-        if(!(solve(root->right,mx,mn) && root->val<mn)){
-            two=false;
-        }
-        int s1=sum;
-        if(!one || !two)return false;
-        sum=root->val+s+s1;
-        ans=max(ans,sum);
-        mx=max(mx,root->val);
-        mn=min(mnn,root->val);
-        return true;
+        if(check==1)ans=max(ans,sum);
+        return {sum,m,mx,check};
     }
-
     int maxSumBST(TreeNode* root) {
-        int mx=INT_MAX;
-        int mn=INT_MIN;
-        solve(root,mx,mn);
+        int ans=0;
+        if(!root)return ans;
+        solve(root,ans);
         return ans;
     }
 };
