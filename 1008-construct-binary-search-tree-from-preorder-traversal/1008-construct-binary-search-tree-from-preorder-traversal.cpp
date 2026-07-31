2888529661c9cf6a6d206e5bd8c6aee8c1f3ebfa
Bhargav Mane <bhargavmane18@gmail.com>
2026-07-31 19:28:23 +0530

    Time: 0 ms (100%), Space: 15.7 MB (99.62%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,17 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int i = 0;
-    TreeNode* build(vector<int>& nums, int bound) {
-        if (i == nums.size() || nums[i] > bound) {
-            return nullptr;
+int i=1;
+    void solve(TreeNode* root,vector<int>&nums,int m,int mx){
+        if(i>=nums.size())return ;
+        if(nums[i]<root->val && nums[i]<mx && nums[i]>m){
+            TreeNode*t=new TreeNode(nums[i]);
+            i++;
+            root->left=t;
+            solve(root->left,nums,m,root->val);
         }
-        TreeNode* root = new TreeNode(nums[i]);
-        i++;
-        root->left = build(nums, root->val);
-        root->right = build(nums, bound);
-        return root;
+        if(i>=nums.size())return ;
+        if(nums[i]>root->val  && nums[i]<mx && nums[i]>m){
+            TreeNode*t=new TreeNode(nums[i]);
+            i++;
+            root->right=t;
+            solve(root->right,nums,root->val,mx);
+        }
     }
     TreeNode* bstFromPreorder(vector<int>& nums) {
-        return build(nums, INT_MAX);
+        TreeNode*root=new TreeNode(nums[0]);
+        solve(root,nums,INT_MIN,INT_MAX);
+        return root;
     }
 };
