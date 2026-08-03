d073a6f77995e03d4f7a07ca2d8af889115ddeb2
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-03 18:01:13 +0530

    Time: 1 ms (94.7%), Space: 23.7 MB (82.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,7 +7,7 @@ public:
         sort(in.begin(),in.end());
         ans.push_back(in[0]);
         for(int i=1;i<n;i++){
-            if(ans[x][0]<=in[i][0] && ans[x][1]>=in[i][0]){
+            if(ans[x][1]>=in[i][0]){
                 ans[x][1]=max(in[i][1],ans[x][1]);
             }
             else {
