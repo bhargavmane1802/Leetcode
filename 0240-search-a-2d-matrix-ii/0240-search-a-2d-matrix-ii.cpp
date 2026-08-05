0fceb37c5f89631e690a0eb72b0a9eaf982e0a9d
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-05 22:59:21 +0530

    Time: 59 ms (30.38%), Space: 18.6 MB (93.37%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,15 +4,13 @@ public:
         int n=nums.size();
         int m=nums[0].size();
         if(t<nums[0][0] || t>nums[n-1][m-1])return false;
-        for(int i=0;i<n;i++){
-            int l=0;
-            int r=m-1;
-            while(l<=r){
-                int mid=l+(r-l)/2;
-                if(nums[i][mid]==t)return true;
-                else if(nums[i][mid]>t)r=mid-1;
-                else l =mid+1;
-            }
+        int l=0;
+        int r=m-1;
+        while(l<n && r>=0){
+            int mid=l+(r-l)/2;
+            if(nums[l][r]==t)return true;
+            else if(nums[l][r]>t)r--;
+            else l++;
         }
         return false;
     }
