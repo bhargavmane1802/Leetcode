c6fe10c1e10a302b197d9afeda5a419e8d60d509
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-10 17:48:57 +0530

    Time: 7 ms (9.18%), Space: 15.4 MB (12.67%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -10,15 +10,15 @@ public:
         }
 
         for(int i=ind;i<can.size();i++){
-            if(i > ind && can[i] == can[i-1])
-            continue;
+            if(s+can[i]>t){
+                return;
+            }
+           if(i > ind && can[i] == can[i-1])
+        continue;
             if(i==0 || i==ind || can[i]!=can[i-1]){
                 temp.push_back(can[i]);
                 cs(i+1,s+can[i],t,can,temp,ans);
                 temp.pop_back();
-            }
-            if(s+can[i]>t){
-                return;
             }
         }
     }
