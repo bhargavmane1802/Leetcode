19f90a057a6aee4e95372a3173cb9983f79eb5dc
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-14 18:15:51 +0530

    Time: 3 ms (31.41%), Space: 11.7 MB (15.53%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& ans) {
-        ans.push_back(curr);
-
-        for (int i = start; i < nums.size(); i++) {
-            if (i > start && nums[i] == nums[i - 1]) continue;
-
-            curr.push_back(nums[i]);
-            solve(nums, i + 1, curr, ans);
-            curr.pop_back();
+    void solve(vector<int>&nums,int idx, vector<vector<int>>&ans,vector<int>&temp){
+        if(idx>nums.size())return;
+        if(idx==nums.size()){
+            ans.push_back(temp);
+            return;
         }
-    }
+        temp.push_back(nums[idx]);
+        solve(nums,idx+1,ans,temp);
+        temp.pop_back();
+        for(int i=idx+1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])continue;
+            temp.push_back(nums[i]);
+            solve(nums,i+1,ans,temp);
+            temp.pop_back();
+        }
+        ans.push_back(temp);
+        return ;
 
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        vector<int> curr;
-        solve(nums, 0, curr, ans);
+        vector<vector<int>>ans;
+        vector<int>temp;
+        sort(nums.begin(),nums.end());
+        solve(nums,0,ans,temp);
         return ans;
     }
 };
