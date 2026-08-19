8e94db1574ff254d66f8e0d427af92c2dbb5a076
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-19 15:42:02 +0530

    Time: 111 ms (14.36%), Space: 87.9 MB (18.24%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& r) {
-
-        unordered_map<int, int> mp;
-
-        for(auto x : r) {
-            int row = x[0];
-            int seat = x[1];
-
-            if(seat >= 2 && seat <= 5)
-                mp[row] |= 1;
-
-            if(seat >= 4 && seat <= 7)
-                mp[row] |= 2;
-
-            if(seat >= 6 && seat <= 9)
-                mp[row] |= 4;
+        unordered_map<int,vector<int>>temp;
+        for(auto x:r){
+            if(temp[x[0]].size()!=0)continue;
+            temp[x[0]]={1,1,1};
         }
-
-        int ans = (n - mp.size()) * 2;
-
-        for(auto [row, mask] : mp) {
-
-            bool left  = !(mask & 1);
-            bool middle = !(mask & 2);
-            bool right = !(mask & 4);
-
-            if(left && right)
+        for(auto x:r){
+            int a=x[1];
+            if(a>=2 && a<=5)temp[x[0]][0]=0;
+            if(a>=4 && a<=7)temp[x[0]][1]=0;
+            if(a>=6 && a<=9)temp[x[0]][2]=0;
+        }
+        int ans=(n-(temp.size()))*2;
+        for(auto x:temp){
+            auto t=x.second;
+            if(t.size()==0 ||(t[0] && t[2]))
                 ans += 2;
-            else if(left || middle || right)
+            else if(t[0] || t[1] || t[2])
                 ans += 1;
-        }
 
+                    }
         return ans;
+        
     }
 };
