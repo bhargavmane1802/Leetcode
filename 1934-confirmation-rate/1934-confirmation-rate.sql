a8d42e20f717490db6e3a0328e2dbd1a0a6d2ae3
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-23 15:22:58 +0530

    Time: 667 ms (79.69%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,13 +1,23 @@
-# Write your MySQL query statement below
-select a.user_id,round(coalesce((coalesce(b.con,0)/coalesce(d.total,0)),0),2) as confirmation_rate 
-from (Signups a left join (
-    select user_id ,coalesce(count(*),0) as con 
-    from Confirmations 
-    where action ='confirmed'
-    group by user_id ,action
-) b on a.user_id=b.user_id ) 
-left join (
-    select user_id ,coalesce(count(*),0) as total 
-    from Confirmations 
-    group by user_id 
-)d on b.user_id =d.user_id;
+-- # Write your MySQL query statement below
+-- select a.user_id,round(coalesce((coalesce(b.con,0)/coalesce(d.total,0)),0),2) as confirmation_rate 
+-- from (Signups a left join (
+--     select user_id ,coalesce(count(*),0) as con 
+--     from Confirmations 
+--     where action ='confirmed'
+--     group by user_id ,action
+-- ) b on a.user_id=b.user_id ) 
+-- left join (
+--     select user_id ,coalesce(count(*),0) as total 
+--     from Confirmations 
+--     group by user_id 
+-- )d on b.user_id =d.user_id;
+
+SELECT 
+    s.user_id,
+    ROUND(COALESCE
+    (SUM(c.action = 'confirmed') / COUNT(c.action),0),2) 
+    AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
