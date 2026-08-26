a6e057f6718fae358835966891ea8d822d5a5f1f
Bhargav Mane <bhargavmane18@gmail.com>
2026-08-27 00:15:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.53%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int n=s.size();
-        vector<int>temp;
-        for(int i=0;i<n;i++){
-            if(s[i]=='1')temp.push_back(i);
-        }
-        if(temp.size()<k)return "";
-        int t=INT_MAX;
-        string ans;
-        for(int i=0;i<=temp.size()-k;i++){
-            int l=temp[i+k-1]-temp[i]+1;
-            string curr=s.substr(temp[i],l);
-            if(l<t){
-                ans=curr;
-                t=l;
-            }
-            else if (l==t && curr<ans){
-                ans=curr;
-            }
-        }
-        
-        
-        return ans;
+        int n = s.size();
 
+        vector<int> temp;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1')
+                temp.push_back(i);
+        }
+
+        if (temp.size() < k)
+            return "";
+
+        int t = INT_MAX;
+        int ansIndex = 0;
+
+        for (int i = 0; i + k - 1 < temp.size(); i++) {
+
+            int len = temp[i + k - 1] - temp[i] + 1;
+
+            if (len < t) {
+                t = len;
+                ansIndex = i;
+            }
+
+            else if (len == t) {
+
+                int a = i + 1;
+                int b = ansIndex + 1;
+
+                while (a <= i + k - 1 &&
+                       temp[a] - temp[a - 1] ==
+                       temp[b] - temp[b - 1]) {
+                    a++;
+                    b++;
+                }
+
+                if (a <= i + k - 1 &&
+                    temp[a] - temp[a - 1] >
+                    temp[b] - temp[b - 1]) {
+                    ansIndex = i;
+                }
+            }
+        }
+
+        return s.substr(temp[ansIndex], t);
     }
 };
