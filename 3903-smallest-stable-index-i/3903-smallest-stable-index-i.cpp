1ec20243b9c5c890884fe71b8dd0a361360e6b40
Bhargav Mane <bhargavmane18@gmail.com>
2026-09-04 13:33:58 +0530

    Time: 0 ms (100%), Space: 31.5 MB (36.87%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>mn(n);
+        vector<int>mx(n);
+        int m=nums[0];
+        for(int i=0;i<n;i++){
+            m=max(nums[i],m);
+            mx[i]=m;
+        }
+        m=nums[n-1];
+        for(int i=n-1;i>=0;i--){
+            m=min(nums[i],m);
+            mn[i]=m;
+        }
+        for(int i=0;i<n;i++){
+            if(mx[i]-mn[i]<=k)return i;
+        }
+        return -1;
+    }
+};
