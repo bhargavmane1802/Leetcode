9398faefcb2cf591acc11335d3e175ffd6190d9f
Bhargav Mane <bhargavmane18@gmail.com>
2026-09-07 21:03:50 +0530

    Time: 3 ms (71.56%), Space: 11.7 MB (73.85%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>q(wordDict.begin(),wordDict.end());
-       vector<int>temp;
-       temp.push_back(0);
-       int n=s.size();
-       for(int i=0;i<n;i++){
-        int a=temp.size();
-        for(int j=0;j<a;j++){
-            string t=s.substr(temp[j],i-temp[j]+1);
-            if(q.find(t)!=q.end()){
-                temp.push_back(i+1);
-                break;
-            }
+bool solve(int i,string &s, vector<string>& word,unordered_set<int>&mp){
+    if(i>=s.size())return true;
+    int n=s.size()-i;
+    if(mp.find(i)!=mp.end())return false;
+    for(string c:word){
+        int x=c.size();
+        if(n<x)continue;
+        string temp=s.substr(i,x);
+        if(temp==c){
+            if(solve(i+x,s,word,mp))
+            return true;
+            mp.insert(i+x);
         }
-       }
-       if(temp.back()==n)return true;
-       return false;
+    }
+    return false;
+}
+    bool wordBreak(string s, vector<string>& word) {
+        unordered_set<int>temp;
+        return solve(0,s,word,temp);
     }
 };
